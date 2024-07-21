68fbec49294b10da56806c4687bd6adf45df3c08
Estebann <estebann17e@gmail.com>
2024-07-20 22:53:27 -0300

    update

--- components/mobile-menu.tsx
+++ components/mobile-menu.tsx
@@ -37,8 +37,8 @@ export default function MobileMenu() {
                         </div>
                     </div>
                     <div className="w-full flex justify-between space-x-2 mt-2 pb-4 px-4">
-                        <Link href="#" className="w-1/2 py-2 px-4 border-2 border-indigo-600 hover:bg-indigo-700 text-gray-100 rounded-md transition duration-300 text-center">Login</Link>
-                        <Link href="#" className="w-1/2 py-2 px-4 bg-indigo-600 hover:bg-indigo-700 text-gray-100 rounded-md transition duration-300 text-center">Signup</Link>
+                        <Link href="#" className="w-1/2 py-2 px-4 border-2 border-[#c026d3] text-gray-100 rounded-md transition duration-300 text-center">Login</Link>
+                        <Link href="#" className="w-1/2 py-2 px-4 bg-[#d946ef] hover:bg-[#D83CEF] text-gray-100 rounded-md transition duration-300 text-center">Signup</Link>
                     </div>
                 </SheetContent>
             </Sheet>
